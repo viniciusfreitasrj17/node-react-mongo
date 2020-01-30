9c101aeba7014b3b6a7e0db377a9e5811e0d718f
Vinicius Freitas <viniciusfreitasrj17@gmail.com>
2020-01-29 22:12:54 -0300

    Front Add 1

--- front/src/routes.js
+++ front/src/routes.js
@@ -8,7 +8,31 @@ const Routes = () => (
           <main>
               <ul>
                   <li>
-                      {/* Aqui */}
+                      <img 
+                        src='https://images-americanas.b2w.io/produtos/01/00/img7/01/00/item/134217/3/134217361_1GG.jpg'
+                        alt='Samsung Galaxy S10'
+                      />
+                      <div className='prod-infp' >
+                        <strong>Smartphone</strong>
+                        <span>Samsung Galaxy S10</span>
+                        <p>128GB Dual Chip Android 9.0 Tela 5,8 Octa-Core 4G Câmera 12MP + 16MP - Preto</p>
+                      </div>
+                      <span>Quantidade 4</span>
+                      <span>R$ 2.300,00</span>
+                  </li>
+
+                  <li>
+                      <img 
+                        src='https://images-americanas.b2w.io/produtos/01/00/img7/01/00/item/134217/3/134217361_1GG.jpg'
+                        alt='Samsung Galaxy S10'
+                      />
+                      <div className='prod-infp' >
+                        <strong>Smartphone</strong>
+                        <span>Samsung Galaxy S10</span>
+                        <p>128GB Dual Chip Android 9.0 Tela 5,8 Octa-Core 4G Câmera 12MP + 16MP - Preto</p>
+                      </div>
+                      <span>Quantidade 4</span>
+                      <span>R$ 2.300,00</span>
                   </li>
               </ul>
           </main>
