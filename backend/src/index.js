4bd3314dfa3428b672b70bb5200ff0c778949571
Vinicius Freitas <viniciusfreitasrj17@gmail.com>
2020-01-31 17:03:59 -0300

    Finish Get Home with API

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,10 +2,9 @@ const port = 4444
 const express = require('express')
 const mongoose = require('mongoose')
 const routes = require('./routes')
+const cors = require('cors')
 
 const app = express()
-
-app.use(express.json())
 
 mongoose.connect('mongodb+srv://marcos:l4cun4@cluster0-hnlwd.mongodb.net/simple?retryWrites=true&w=majority', {
   useNewUrlParser: true,
@@ -13,6 +12,8 @@ mongoose.connect('mongodb+srv://marcos:l4cun4@cluster0-hnlwd.mongodb.net/simple?
   useCreateIndex: true
 })
 
+app.use(cors({ origin: 'http://localhost:3000' }))
+app.use(express.json())
 app.use(routes)
 
 app.listen(process.env.PORT || port, () => console.log(`Listening on port: ${port}`))
--- front/src/routes.js
+++ front/src/routes.js
@@ -1,124 +1,124 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import api from './service/api'
 
 import './Home.css'
 import './Admin.css'
 
-const Routes = () => (
-  <BrowserRouter>
-    <Switch>
-      <Route exact path='/' component={() => (
-        <main id='home' >
-          <ul>
-            <li>
-              <img
-                src='https://images-americanas.b2w.io/produtos/01/00/img7/01/00/item/134217/3/134217361_1GG.jpg'
-                alt='Samsung Galaxy S10'
-              />
-              <header>
-                <div className='prod-info' >
-                  <strong>Smartphone</strong>
-                  <span>Samsung Galaxy S10</span>
-                  <p>128GB Dual Chip Android 9.0 Tela 5,8 Octa-Core 4G Câmera 12MP + 16MP - Preto</p>
-                </div>
-                <div className='prod-add' >
-                  <span id='qtd' >Quantidade 4</span>
-                  <span id='price' >R$ 2.300,00</span>
-                </div>
-              </header>
-            </li>
+const Routes = () => {
+  const [prod, setProd] = useState([])
 
-            <li>
-              <img
-                src='https://images-americanas.b2w.io/produtos/01/00/img7/01/00/item/134217/3/134217361_1GG.jpg'
-                alt='Samsung Galaxy S10'
-              />
-              <header>
-                <div className='prod-info' >
-                  <strong>Smartphone</strong>
-                  <span>Samsung Galaxy S10</span>
-                  <p>128GB Dual Chip Android 9.0 Tela 5,8 Octa-Core 4G Câmera 12MP + 16MP - Preto</p>
-                </div>
-                <div className='prod-add' >
-                  <span id='qtd' >Quantidade 4</span>
-                  <span id='price' >R$ 2.300,00</span>
-                </div>
-              </header>
-            </li>
-          </ul>
-        </main>
-      )} />
-      <Route exact path='/admin' component={() => (
-        <main id='admin' >
-          <strong>Cadastro de Produto</strong>
-          <form onSubmit={() => { }} >
-            <div className='input-block' >
-              <label htmlFor='name' >Nome do Produto</label>
-              <input
-                name='name'
-                id='name'
-                required
-                value=''
-              />
-            </div>
+  useEffect(() => {
+    async function loadProd() {
+      const { data } = await api.get('/prod')
 
-            <div className='input-block' >
-              <label htmlFor='description' >Descrição do Produto</label>
-              <input
-                name='description'
-                id='description'
-                required
-                value=''
-              />
-            </div>
+      setProd(data)
+    }
 
-            <div className='input-block' >
-              <label htmlFor='detail' >Detalhes</label>
-              <textarea
-                name='detail'
-                id='detail'
-                required
-                rows='2'
-                cols='33'
-                value=''
-              />
-            </div>
+    loadProd()
+  }, [])
 
-            <div className='input-block' >
-              <label htmlFor='imgUrl' >Link da Imagem</label>
-              <input
-                name='imgUrl'
-                id='imgUrl'
-                required
-                value=''
-              />
-            </div>
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path='/' component={() => (
+          <main id='home' >
+            <ul>
+              {prod.map(p => (
+                <li>
+                  <img
+                    src={p.imgUrl}
+                    alt={p.name}
+                  />
+                  <header>
+                    <div className='prod-info' >
+                      <strong> {p.name} </strong>
+                      <span> {p.description} </span>
+                      <p> {p.detail} </p>
+                    </div>
+                    <div className='prod-add' >
+                      {(p.amount > 0 ? <span id='qtd-none' /> : <span id='qtd'>FORA DO ESTOQUE</span>)}
+                      <span id='price' > {`R$ ${p.price}`} </span>
+                    </div>
+                  </header>
+                </li>
+              ))}
+            </ul>
+          </main>
+        )} />
+        <Route exact path='/admin' component={() => (
+          <main id='admin' >
+            <strong>Cadastro de Produto</strong>
+            <form onSubmit={() => { }} >
+              <div className='input-block' >
+                <label htmlFor='name' >Nome do Produto</label>
+                <input
+                  name='name'
+                  id='name'
+                  required
+                  value=''
+                />
+              </div>
 
-            <div className='input-block' >
-              <label htmlFor='price' >Preço do Produto</label>
-              <input
-                name='price'
-                id='price'
-                required
-                value=''
-              />
-            </div>
+              <div className='input-block' >
+                <label htmlFor='description' >Descrição do Produto</label>
+                <input
+                  name='description'
+                  id='description'
+                  required
+                  value=''
+                />
+              </div>
 
-            <div className='input-block' >
-              <label htmlFor='amount' >Quantidade</label>
-              <input
-                name='amount'
-                id='amount'
-                value=''
-              />
-            </div>
+              <div className='input-block' >
+                <label htmlFor='detail' >Detalhes</label>
+                <textarea
+                  name='detail'
+                  id='detail'
+                  required
+                  rows='2'
+                  cols='33'
+                  value=''
+                />
+              </div>
 
-            <button type='submit' >Cadastrar</button>
-          </form>
-        </main>
-      )} />
-    </Switch>
-  </BrowserRouter>
-)
+              <div className='input-block' >
+                <label htmlFor='imgUrl' >Link da Imagem</label>
+                <input
+                  name='imgUrl'
+                  id='imgUrl'
+                  required
+                  value=''
+                />
+              </div>
+
+              <div className='input-block' >
+                <label htmlFor='price' >Preço do Produto</label>
+                <input
+                  name='price'
+                  id='price'
+                  required
+                  value=''
+                />
+              </div>
+
+              <div className='input-block' >
+                <label htmlFor='amount' >Quantidade</label>
+                <input
+                  name='amount'
+                  id='amount'
+                  value=''
+                />
+              </div>
+
+              <button type='submit' >Cadastrar</button>
+            </form>
+          </main>
+        )} />
+        <Route exact path='/api' component={() => <>{prod.map(p => <div>{Object.entries(p)}</div>)}</>} />
+      </Switch>
+    </BrowserRouter>
+  )
+}
 
 export default Routes
