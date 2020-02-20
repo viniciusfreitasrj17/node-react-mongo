cce3b1a425f6b27c4340725aa76e16c6152b114a
Vinicius Freitas <viniciusfreitasrj17@gmail.com>
2020-02-19 22:11:40 -0300

    Add Init Window Update

--- front/src/components/Admin/index.jsx
+++ front/src/components/Admin/index.jsx
@@ -1,15 +1,72 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import './styles.css'
-import { Link } from 'react-router-dom'
+import Popup from 'reactjs-popup'
+
+function PopupEx() {
+
+    return (
+<Popup trigger={<button className="updateProduto"> Editar </button>} modal>
+{close => (
+  <div className="modal">
+    <a className="close" onClick={close}>
+      &times;
+    </a>
+    <div className="header"> Modal Title </div>
+    <div className="content">
+      {" "}
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, a nostrum.
+      Dolorem, repellat quidem ut, minima sint vel eveniet quibusdam voluptates
+      delectus doloremque, explicabo tempore dicta adipisci fugit amet dignissimos?
+      <br />
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur sit
+      commodi beatae optio voluptatum sed eius cumque, delectus saepe repudiandae
+      explicabo nemo nam libero ad, doloribus, voluptas rem alias. Vitae?
+    </div>
+    <div className="actions">
+      <Popup
+        trigger={<button className="button"> Trigger </button>}
+        position="top center"
+        closeOnDocumentClick
+      >
+        <span>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae
+          magni omnis delectus nemo, maxime molestiae dolorem numquam
+          mollitia, voluptate ea, accusamus excepturi deleniti ratione
+          sapiente! Laudantium, aperiam doloribus. Odit, aut.
+        </span>
+      </Popup>
+      <button
+        className="button"
+        onClick={() => {
+          console.log("modal closed ");
+          close();
+        }}
+      >
+        close modal
+      </button>
+    </div>
+  </div>
+)}
+</Popup>
+    )
+}
 
 function Admin(props) {
+    let [displayUp, setDisplayUp] = useState(false)
+
     const { p, delItem, upItem } = props
 
     async function deleteItem(e) {
         e.preventDefault()
 
         await delItem(p._id)
+    }
+
+    function dispUp(e) {
+        e.preventDefault()
+
+        setDisplayUp(!displayUp)
     }
 
     return (
@@ -19,9 +76,7 @@ function Admin(props) {
                     X
                 </button>
             </form>
-            <form>
-                <Link id='updateProduto' to={{ pathname: '/update', params: { fromNotifications:true } }} > Editar </Link>
-            </form>
+                <PopupEx />
             <img
                 src={p.imgUrl}
                 alt={p.name}
